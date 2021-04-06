26883d4f90e9f24f9c01b3e815b714bbe76e884b
wlhappyday <1601995422@qq.com>
2021-04-06 14:23:34 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/api/controller/Login.php
    #	app/common.php
    #	config/jwt.php

--- app/middleware/Auth.php
+++ app/middleware/Auth.php
@@ -9,16 +9,15 @@ class Auth
 {
     /**
      * 处理请求
-     *
      * @param  $request
      * @param Closure $next
      * @return mixed $next
      */
     public function handle($request, Closure $next)
     {
-//        if($request->server()['REQUEST_URI']!="/api/login/login"){
-//                JWTAuth::auth();
-//        }
+        if($request->server()['REQUEST_URI']!="/api/login/login"){
+                JWTAuth::auth();
+        }
         return $next($request);
     }
 }
