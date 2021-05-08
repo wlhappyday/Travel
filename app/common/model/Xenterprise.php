2ce11a7ea2c6fa32fb50b641d6d9ca2e30f59553
wlhappyday <1601995422@qq.com>
2021-05-08 11:11:00 +0800

    数据库

---
+++ app/common/model/Xenterprise.php
@@ -0,0 +1,19 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\common\model;
+
+use think\Model;
+use hg\apidoc\annotation\Field;
+use hg\apidoc\annotation\WithoutField;
+use hg\apidoc\annotation\AddField;
+use think\model\concern\SoftDelete;
+
+/**
+ * @mixin \think\Model
+ */
+class Xenterprise extends Model
+{
+    use SoftDelete;
+    protected $name = 'x_enterprise';
+}
