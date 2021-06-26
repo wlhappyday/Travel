e625a42d5c7e9dc0350de31b31fb523ad169e997
wlhappyday <1601995422@qq.com>
2021-06-27 02:09:18 +0800

    Merge remote-tracking branch 'origin/main'

--- app/common.php
+++ app/common.php
@@ -18,7 +18,10 @@ use app\common\model\XuserBalanceRecords;
 use app\common\model\XuserLog;
 use app\platform\model\Product_relation;
 use thans\jwt\facade\JWTAuth;
-use think\facade\Db;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\DbException;
+use think\db\exception\ModelNotFoundException;
+use think\response\Json;
 
 function p($arr)
 {
@@ -33,7 +36,8 @@ function p($arr)
  * @param $length
  * @return null|string
  */
-function get_rand_char($length){
+function get_rand_char($length): ?string
+{
     $str = null;
     $strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
     $max = strlen($strPol) - 1;
@@ -122,7 +126,6 @@ function post($url, $data)
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    //   curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
     $output = curl_exec($ch);
     //   var_dump(curl_error($ch));die();
     curl_close($ch);
@@ -130,29 +133,32 @@ function post($url, $data)
 }
 
 /**
- * @param $passwd   明文密码
+ * @param $passwd
  * @return array
  * @author WjngJiamao
  * @Note   密码加密
  */
-function encryptionPasswd($passwd)
+function encryptionPasswd($passwd): array
 {
     $passwdSalt = get_rand_char(20);
 
     return ['passwd' => md5($passwdSalt . $passwd), 'passwd_salt' => $passwdSalt];
 }
+
 /**
+ * @param string $passwd 用户提交的明文密码
+ * @param array $data 用户数据 包含 加密后的密码、盐值
+ * @return bool
  * @author WjngJiamao
  * @Note  验证密码
- * @param string $passwd    用户提交的明文密码
- * @param array  $data      用户数据 包含 加密后的密码、盐值
- * @return bool
  */
-function checkPasswd($passwd, $data)
+function checkPasswd(string $passwd, array $data): bool
 {
     return $data['passwd'] == md5($data['passwd_salt'] . $passwd);
 }
-function returnData(array $data,int $code = 200,array $header=[]){
+
+function returnData(array $data, int $code = 200, array $header = []): Json
+{
     return json($data)->code($code)->header($header);
 }
 
@@ -160,7 +166,7 @@ function returnData(array $data,int $code = 200,array $header=[]){
  * @author WjngJiamao
  * @Note  获取请求头中的Token并解析
  */
-function getDecodeToken()
+function getDecodeToken(): array
 {
     $payload = JWTAuth::auth();
     $date = [];
@@ -174,7 +180,7 @@ function getDecodeToken()
  * @throws ClientException
  * @throws ServerException
  */
-function aliSms($date, $phone, $TemplateCode = "SMS_202815323")
+function aliSms($date, $phone, $TemplateCode = "SMS_202815323"): array
 {
     AlibabaCloud::accessKeyClient("LTAI4G4m3pm6GzdcdWQSfs9m", "rFWJ721dapSvuUxqQR7oyN0aesiOHh")
         ->regionId('cn-hangzhou')
@@ -213,13 +219,13 @@ function isUserToken($data, $type)
         return false;
     }
     if ($type == '1') {
-        $data = app\api\model\Admin::where(['id' => $data['id'], 'phone' => $data['phone']])->value('id');
+        $data = (new app\api\model\Admin)->where(['id' => $data['id'], 'phone' => $data['phone']])->value('id');
     } elseif ($type == '2') {
-        $data = app\api\model\Padmin::where(['id'=>$data['id'],'phone'=>$data['phone'],'status'=>'0'])->value('id');
+        $data = (new app\api\model\Padmin)->where(['id' => $data['id'], 'phone' => $data['phone'], 'status' => '0'])->value('id');
     }elseif ($type == '3'){
-        $data = app\api\model\Juser::where(['id'=>$data['id'],'phone'=>$data['phone'],'status'=>'0'])->value('id');
+        $data = (new app\api\model\Juser)->where(['id' => $data['id'], 'phone' => $data['phone'], 'status' => '0'])->value('id');
     }elseif ($type == '4'){
-        $data = app\api\model\Xuser::where(['id'=>$data['id'],'phone'=>$data['phone'],'status'=>'0'])->value('id');
+        $data = (new app\api\model\Xuser)->where(['id' => $data['id'], 'phone' => $data['phone'], 'status' => '0'])->value('id');
     }
     if($data){
         return true;
@@ -231,8 +237,9 @@ function isUserToken($data, $type)
 
 /**
  * @Note   增加日志记录
- * @param $data   token解密信息
- * @param $info   操作内容
+ * @param $data
+ * @param $info
+ * @return int|string
  */
 function addXuserLog($data,$info){
     $x_user_log = new XuserLog();
@@ -259,9 +266,8 @@ function addPuserLog($data,$info){
  * @author xi 2019/5/23 12:44
  * @Note
  */
-function getIp($type='') {
-    static $realip;
-    $city='';
+function getIp($type = ''): array
+{
     if (isset($_SERVER)) {
         if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $realip = $_SERVER['HTTP_X_FORWARDED_FOR'];
@@ -281,35 +287,37 @@ function getIp($type='') {
     }
     if(empty($type)){
         if($realip=='::1'||$realip=='127.0.0.1'){
-            $realip='127.0.0.1';
-            $city=getCity();
-        }else{
-            $city=getCity($realip);
+            $realip = '127.0.0.1';
+            $city = getCity();
+        } else {
+            $city = getCity($realip);
         }
-        return ['ip'=>$realip,'address'=>$city['city'],'province'=>$city['province']];
+        return ['ip' => $realip, 'address' => $city['city'], 'province' => $city['province']];
     }
-    return ['ip'=>$realip];
+    return ['ip' => $realip];
 
 }
-function getCity($ip = '')//获取地区
+
+function getCity($ip = ''): array//获取地区
 {
-    $url = "http://api.map.baidu.com/location/ip?ak=M7Mc1jF8vmzGNx7XL1TAgHbBWB8oyuwv&ip=".$ip;
-    $ip=json_decode(file_get_contents($url),true);
-    if(!isset($ip['content'])){
-        return ['city'=>'未知','province'=>'未知'];
+    $url = "http://api.map.baidu.com/location/ip?ak=M7Mc1jF8vmzGNx7XL1TAgHbBWB8oyuwv&ip=" . $ip;
+    $ip = json_decode(file_get_contents($url), true);
+    if (!isset($ip['content'])) {
+        return ['city' => '未知', 'province' => '未知'];
     }
-    if(!isset($ip['content']['address'])){
-        return ['city'=>'未知','province'=>'未知'];
+    if (!isset($ip['content']['address'])) {
+        return ['city' => '未知', 'province' => '未知'];
     }
     $data['city'] = $ip['content']['address'];
     $data['province'] = $ip['content']['address_detail']['province'];
     return $data;
 }
 //获取域名
-function http(){
+function http(): string
+{
     $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
     $SERVER_NAME = $_SERVER['SERVER_NAME'];
-    return $http_type.$SERVER_NAME;
+    return $http_type . $SERVER_NAME;
 }
 
 //GET提交
@@ -326,59 +334,66 @@ function http(){
     }
 
 //创建产品审核
-function ProductReviewAdd($data,$product_id){
+function ProductReviewAdd($data, $product_id): array
+{
     $product_result = new Jproduct();
-    $uid = $product_result->where(['id'=>$product_id,'type'=>'1','mp_id'=>'6','status'=>'0'])->value('uid');
-    if (!$uid){
-        return ['msg'=>'该产品不符合规则','code'=>'201'];
+    $uid = $product_result->where(['id' => $product_id, 'type' => '1', 'mp_id' => '6', 'status' => '0'])->value('uid');
+    if (!$uid) {
+        return ['msg' => '该产品不符合规则', 'code' => '201'];
     }
 
     $result = new JproductReview();
-    $res = $result->where(['uid'=>$uid,'product_id'=>$product_id,'pid'=>$data['id']])->whereIn('state',[1,2])->value('id');
-    if($res){
-        return ['msg'=>'已存在该产品','code'=>'201'];
+    $res = $result->where(['uid' => $uid, 'product_id' => $product_id, 'pid' => $data['id']])->whereIn('state', [1, 2])->value('id');
+    if ($res) {
+        return ['msg' => '已存在该产品', 'code' => '201'];
     }
-    Db::startTrans();
+    (new think\facade\Db)->startTrans();
     try {
         $result->insert(['product_id' => $product_id, 'uid' => $uid, 'pid' => $data['id'], 'create_time' => time()]);
         addPadminLog($data, '创建产品审核：' . $data['id']);
-        Db::commit();
+        (new think\facade\Db)->commit();
         return ['msg' => '创建成功', 'code' => '200'];
     } catch (Exception $e) {
-        Db::rollback();
+        (new think\facade\Db)->rollback();
         return ['code' => '201', 'msg' => '网络异常'];
     }
 }
 
-    function product_relation($pid,$product_id,$Review_id){
-        $j_product = Jproduct::where(['status'=>'0','id'=>$product_id,'mp_id'=>'6'])->find();
-        if ($j_product){
-            Db::startTrans();
-            try {
-                $productReview = JproductReview::find($Review_id);
-                $productReview->state = 2;
-                $productReview->update_time = time();
-                $productReview->save();
-                $product_relation = new Product_relation();
-                if ($productReview['state']=='2'){
-                    $relation = Product_relation::where(['uid'=>$pid,'product_id'=>$product_id])->find();
+/**
+ * @throws ModelNotFoundException
+ * @throws DataNotFoundException
+ * @throws DbException
+ */
+function product_relation($pid, $product_id, $Review_id): array
+{
+    $j_product = (new app\common\model\Jproduct)->where(['status' => '0', 'id' => $product_id, 'mp_id' => '6'])->find();
+    if ($j_product) {
+        (new think\facade\Db)->startTrans();
+        try {
+            $productReview = (new app\common\model\JproductReview)->find($Review_id);
+            $productReview->state = 2;
+            $productReview->update_time = time();
+            $productReview->save();
+            $product_relation = new Product_relation();
+            if ($productReview['state'] == '2') {
+                $relation = (new app\platform\model\Product_relation)->where(['uid' => $pid, 'product_id' => $product_id])->find();
                     if ($relation){
                         return ['code'=>'201','msg'=>'已绑定该产品'];
                     }
-                    $product_relation->save([
-                        'uid'  =>  $pid,
-                        'type' => $j_product['type'],
-                        'product_id' => $product_id,
-                        'price' => $j_product['money'],
-                        'mp_id' => $j_product['mp_id']
-                    ]);
-                    Db::commit();
+                $product_relation->save([
+                    'uid' => $pid,
+                    'type' => $j_product['type'],
+                    'product_id' => $product_id,
+                    'price' => $j_product['money'],
+                    'mp_id' => $j_product['mp_id']
+                ]);
+                (new think\facade\Db)->commit();
                     return ['code' => '200', 'msg' => '操作成功'];
                 } else {
                     return ['code' => '201', 'msg' => '该产品未审核'];
                 }
             } catch (Exception $e) {
-                Db::rollback();
+            (new think\facade\Db)->rollback();
                 return ['code' => '201', 'msg' => '网络异常'];
             }
         }else{
@@ -388,15 +403,15 @@ function ProductReviewAdd($data,$product_id){
     }
 
 /**
- * @author LvGang
- * @Note   获取系统变量
- *
  * @param string $name
  *
  * @return array
+ * @author LvGang
+ * @Note   获取系统变量
+ *
  */
-function getVariable($name = '')
+function getVariable(string $name = ''): array
 {
     $result = new Config();
-    return $result->where(['title'=>$name])->value('value');
+    return $result->where(['title' => $name])->value('value');
 }
